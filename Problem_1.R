02b402b0cbd4d6a7f01d56f6ea8ad1e400cb6c55
MariaRHristova <36930541+MariaRHristova@users.noreply.github.com>
2018-03-30 18:45:08 +0300

    :)

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,29 @@
+#a 
+x<- c(4,1,1,4)
+
+#b
+y<-c(1,4)
+
+#c
+x-y
+# The two vectors have different lengths. so the shorter one is "recycled", that means its values are repeated. 
+# R calculates the following:(4-1, 1-4, 1-1, 4-4). (Ne razchitashe kirilica, zatova e na anglijski.) 
+
+#d
+s <- c(x, y)
+
+#e
+rep(s,10)
+
+length(rep(s,10))
+
+#f
+rep(s, each=3)
+
+#g
+seq(7,21,by=1)
+
+7:21
+#h
+length(seq(7,21,by=1))
 
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,50 @@
+#a
+xmin<-c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmax<-c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+
+#b
+xmin-xmax
+
+#c
+mean<-mean(xmin)
+mean(xmax)
+
+#d
+xmin[xmin <mean(xmin)]
+
+#e
+xmax[xmax>mean(xmax)]
+
+#f
+date<-
+names(xmin) <- c( '03Mon18', '04Tue18', '05Wed18', '04Thu18', '05Fri18', '06Sat18', '07Sun18')
+names(xmax)<-  c( '03Mon18', '04Tue18', '05Wed18', '04Thu18', '05Fri18', '06Sat18', '07Sun18')
+xmin
+xmax
+-
+
+
+#g
+temperatures<-data.frame(df.xmin =xmin,df.xmax =xmax)
+
+temperatures
+
+#h
+temperatures$df.xminFahrenheit<-c(xmin)*9/5+32
+temperatures
+
+#i
+temperaturesF<-data.frame(
+  df.tempF =temperatures$df.xminFahrenheit)
+
+#J
+temperaturesF
+temperaturesF5 <- data.frame(
+  df.tempF =temperatures$df.xminFahrenheit[1:5])
+temperaturesF5
+temperaturesF
+temperaturesFlast <- data.frame(
+  df.tempF =temperatures$df.xminFahrenheit[-c(6, 7)])
+
+  temperaturesFlast
 
